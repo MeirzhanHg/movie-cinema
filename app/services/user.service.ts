22bbe64f570d08eb74172ab1a00534549faa4010
MeirzhanHg <mikimikomiko03@gmail.com>
2023-05-27 20:32:56 +0600

    LESSON 27, Update profile user

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,6 +1,8 @@
 import axios from 'api/interceptors'
 import { getUsersUrl } from 'config/api.config'
 
+import { IProfileInput } from '@/components/screens/profile/profile.interface'
+
 import { IUser } from '@/shared/types/user.types'
 
 export const UserService = {
@@ -14,7 +16,23 @@ export const UserService = {
 		})
 	},
 
+	async getProfile() {
+		return axios.get<IUser>(getUsersUrl('/profile'))
+	},
+
+	async updateProfile(data: IProfileInput) {
+		return axios.put<string>(getUsersUrl('/profile'), data)
+	},
+
 	async deleteUser(_id: string) {
 		return axios.delete<string>(getUsersUrl(`/${_id}`))
+	},
+
+	async getById(_id: string) {
+		return axios.get<IUser>(getUsersUrl(`/${_id}`))
+	},
+
+	async update(_id: string, data: IProfileInput) {
+		return axios.put<string>(getUsersUrl(`/${_id}`), data)
 	},
 }
